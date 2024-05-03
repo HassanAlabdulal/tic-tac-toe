a22fa7ce2b1cac4c63d6f03cc6ffc870af5746df
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-05-03 23:14:52 +0300

    start

---
+++ app/src/main/java/sa/kfupm/swe463project/MainActivity.java
@@ -0,0 +1,14 @@
+package sa.kfupm.swe463project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
